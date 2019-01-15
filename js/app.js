7864782cc735cda70a680fe0567b767fbd9de5ee
Asad Ullah Khalid <asadkhalid305@icloud.com>
2019-01-15 20:17:31 +0500

    add, delete and edit work pertfectly

--- js/app.js
+++ js/app.js
@@ -1,45 +1,154 @@
 // import 'bootstrap';
 
+$(function () {
+    $('#addTaskAlert').hide();
+    $('#editTaskAlert').hide();
 
 
-$(document).ready(function () {
     // variables decleared
-    var addtaskInput;
-    var lenChecklist = 2;
+    var taskCount = 1;
+    var tasks = ['item1'];
+    var editItemId = '';
 
-    //modal add button
-    $('#btnAddTask').click(function () {
-        //get no of items in checklist
-        // lenChecklist = $('.checklist .item').length;
-        console.log('Before' + lenChecklist);
 
-        //get user input from modal
-        addtaskInput = $('#inputlg').val();
-        // console.log(addtaskInput);
+    //global work starts
 
-        //make a copy of last inserted item
-        var cloneTask = $("#item" + lenChecklist).clone(true);
-        cloneTask.attr('id', 'item' + (lenChecklist + 1));
+    //modal Add Task - Done button
+    function clickAndEnterAdd() {
+        //atleast one task already present
+        if (tasks.length > 0) {
+            //increase no of tasks in checklist
+            taskCount++;
 
-        //editing content of copied item
-        var editLabel = cloneTask.find('label');
-        editLabel.html(addtaskInput);
+            //make a clone of last inserted item
+            var cloneTask = $('#' + tasks[tasks.length - 1]).clone(true);
+            console.log('Cloned item: ' + cloneTask);
+            cloneTask.attr('id', 'item' + (taskCount));
 
-        //add task in checklist
-        $('.checklist').prepend(cloneTask);
-        lenChecklist++;
-        console.log('After' + lenChecklist);
+            //editing content of cloned item
+            var userInput = $('#addInput').val();
+            var editLabel = cloneTask.find('label');
+            if (userInput === '') {
+                alert("Please enter some content");
+            } else {
+                editLabel.html(userInput);
 
-        //close modal
-        $('#exampleModalCenter').modal('toggle');
+                //add cloned task in checklist
+                $('.checklist').prepend(cloneTask);
+
+                //update tasks array with cloned item
+                tasks.push('item' + taskCount)
+
+                // console.log('After: ' + taskCount);
+                // console.log('After: ' + tasks);
+
+                //close modal
+                $('#addModal').modal('toggle');
+
+                //show alert success
+                $('#addTaskAlert').show();
+            }
+
+        } else {
+            alert('Cannot cloned item');
+
+            //close modal
+            $('#addModal').modal('toggle');
+        }
+    }
+
+    //modal Edit Task - Done button
+    function clickAndEnterEdit() {
+        //get user input
+        var userInput = $('#editInput').val();
+        //editing content of edit item
+        if (userInput === '') {
+            alert("Please enter some content");
+        } else {
+            $('#' + editItemId).find('label').html(userInput);
+
+            //close modal
+            $('#editModal').modal('toggle');
+
+            //show alert success
+            $('#editTaskAlert').show();
+
+        }
+    }
+
+    //get id of clicked item for edit and option popover 
+    $(document).on('click', '.btnEdit, .btnOptionPop', function () {
+        editItemId = $(this).parent().parent().attr('id');
+        console.log(editItemId);
     });
 
-
-    $('.btnDelete').click(function () {
-        console.log('Before ' + lenChecklist);
-        $(this).parent().parent().remove();
-        lenChecklist--;
-        console.log('After ' + lenChecklist);
-        // $(this).remove();
+    //initialize and set content of option popover
+    $('#btnPopover').popover({
+        tabIndex: 0,
+        placement: 'left',
+        trigger: 'hover',
+        title: 'Options',
+        container: 'body',
+        html: true,
+        content: '<div class=""><button type="button" class="btn btn-success btnEditPop" data-toggle="modal" data-target="#editModal">Edit</button><button type="button" class="btn btn-danger" id="btnDeletePop">Delete</button></div>'
     });
+
+    //global work ends
+
+
+    //Edit and Add Task done button click and enter event start
+    $('#btnAddTask').click(
+        function () {
+            clickAndEnterAdd();
+        }
+    );
+
+    $('#btnEditTask').click(
+        function () {
+            clickAndEnterEdit();
+        }
+    );
+
+    $('#addInput').keypress(
+        function (e) {
+            if (e.which === 13) {
+                clickAndEnterAdd();
+            }
+        }
+    );
+
+    $('#editInput').keypress(
+        function (e) {
+            if (e.which === 13) {
+                clickAndEnterEdit();
+            }
+        }
+    );
+    //Edit and Add Task done button click and enter event end
+
+
+    //delete buttons start
+    //delete button from large screen
+    $('.btnDelete').click(
+        function () {
+            var itemId = $(this).parent().parent().attr('id');
+
+            var temp = tasks.indexOf(itemId);
+            tasks.splice(temp, 1);
+
+            $(this).parent().parent().remove();
+            console.log('After deletion: ' + tasks);
+        }
+    );
+
+    //delete button from small screen
+    $('#btnPopover').on('shown.bs.popover', function () {
+        // set what happens when user clicks on the button
+        $("#btnDeletePop").on('click', function () {
+            var temp = tasks.indexOf(editItemId);
+            tasks.splice(temp, 1);
+            $('#' + editItemId).remove();
+        });
+    });
+    //delete buttons end 
 });
